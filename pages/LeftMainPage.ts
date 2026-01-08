c139b2117e32df5308f99c3db980e437c4e204b5
Daria Olsson <dolssontest@gmail.com>
2026-01-07 20:24:04 -0500

    Add: 4 tests to product-landing-page-feature

--- pages/LeftMainPage.ts
+++ pages/LeftMainPage.ts
@@ -15,5 +15,15 @@ export class LeftMainPage extends BasePage {
   public readonly programName: Locator 
     = this.locator("//p[@class='course-name']/a");
     
+  public readonly termsAndConditions: Locator 
+    = this.locator("//a[contains(text(), 'Terms and conditions')]");
 
+  public readonly privacyPolicy: Locator 
+    = this.locator("//a[contains(text(), 'Privacy Policy')]");
+
+  public readonly disclaimer: Locator 
+    = this.locator("//a[contains(text(), 'Disclaimer')]");
+
+  public readonly cookiePolicy: Locator 
+    = this.locator("//a[contains(text(), 'Cookie Policy')]");
 }
